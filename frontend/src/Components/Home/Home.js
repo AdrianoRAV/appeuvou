0fdd643c663981144d398707caf321d808a6a332
Luiz Felipe <Luiz.99421@ymail.com>
2021-04-08 21:19:04 -0300

    Página inicial

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+  
+  return(
+    <>
+      <Header />
+
+      <div>
+        Home
+      </div>
+    </>
+  );
+};
+
+export default Home;
